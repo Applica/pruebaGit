b97c663aa9f325c0bcc700a1e5e68ee2cecd0693
Applica <applica.desarrollos@gmail.com>
2016-01-25 10:00:07 -0300

    Update index.php

--- pruebaGit/index.php
+++ pruebaGit/index.php
@@ -12,6 +12,7 @@ and open the template in the editor.
     <body>
         <?php
             echo "mensaje de Alexis Railen ";
+            echo "mensaje 2 de Alexis Railen  ";
         ?>
     </body>
 </html>
